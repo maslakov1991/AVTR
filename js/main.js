1f66a1f96df1a8d3e2df8b647740eee65a89ffeb
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-01-23 16:18:50 +0200

    Pre-adaptive
    Pre-adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-$(function(){
-
+$(function () {
+    $('.design__slider').slick({
+        slidesToShow: 4,
+        variableWidth: true,
+        centerMode: true,
+        prevArrow: '<img class="arrow arrow-prev" src="images/arrow-prev.svg" alt="">',
+        nextArrow: '<img class="arrow arrow-next" src="images/arrow-next.svg" alt="">'
+    });
 });
