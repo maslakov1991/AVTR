bbbf6020493f34f4e0f8beeeb1f2e5303ab5b18f
Maslakov Olskeii <a.maslakov1991@gmail.com>
2023-01-24 11:50:29 +0200

    Adaptive-done
    Adaptive-done

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@ $(function () {
         variableWidth: true,
         centerMode: true,
         prevArrow: '<img class="arrow arrow-prev" src="images/arrow-prev.svg" alt="">',
-        nextArrow: '<img class="arrow arrow-next" src="images/arrow-next.svg" alt="">'
+        nextArrow: '<img class="arrow arrow-next" src="images/arrow-next.svg" alt="">',
+        responsive: [
+            {
+                breakpoint: 521,
+                settings: {
+                    variableWidth: false,
+                    slidesToShow: 1,
+                    centerMode: false
+                }
+            }
+        ]
     });
 });
